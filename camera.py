69b158dad7c69b2300b6eaa9a27db7eaa444e39c
Lưu Gia Huy <luucoder@protonmail.com>
2022-05-17 23:32:29 +0700

    patch 9

--- camera.py
+++ camera.py
@@ -5,19 +5,21 @@ import serial
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon = 0.8, maxHands = 2)
+serialcomm = serial.Serial('COM3', 9600)
+serialcomm.timeout = 1
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
     if hands:
         hand1 = hands[0]
         fingers1 = detector.fingersUp(hand1)
-        print(fingers1)
-        if len(hands) == 2:
-            hand2 = hands[1]
-            fingers2 = detector.fingersUp(hand2)
-            print(fingers2)
-
-
+        s = '$' + str(fingers1[0]) + str(fingers1[1]) + str(fingers1[2]) + str(fingers1[3]) + str(fingers1[4]);
+        print(fingers1);
+        serialcomm.write(s.encode())
+        # print(serialcomm.readline().decode('ascii'))
     cv2.imshow("image", img)
     key = cv2.waitKey(1)
-    
+    if key == ord('q'): break
+cap.release()
+cv2.destroyAllWindows
+serialcomm.close()
--- uno/uno.ino
+++ uno/uno.ino
@@ -8,21 +8,33 @@ bool counterStart = false;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-   thumb.attach(7);
+  thumb.attach(7);
+  index.attach(8);
+  middle.attach(9);
+  ring.attach(10);
+  pinky.attach(11);
 }
-
+void move(int i, char c){
+  if (i == 1) {
+    if (c == '1') thumb.write(180);
+    else thumb.write(0);
+  }
+  else if (i == 2) index.write(180);
+  else if (i == 3) middle.write(180);
+  else if (i == 4) ring.write(180);
+  else if (i == 5) pinky.write(180);
+  delay(500);
+  thumb.write(90);
+  index.write(90);
+  middle.write(90);
+  ring.write(90);
+  pinky.write(90);
+}
 void loop() {
   if(Serial.available() > 0){
-    char c = Serial.read();
-    if (c == '$'){
-      received = "";
-      counter = 0;
-    }
-    if (c == '1') { 
-      thumb.writeMicroseconds(1500);
-    }
-    if (c == '0' ){
-    }
-    counter++;
+    String s = Serial.readStringUntil('\n');
+    for (int i = 1; i <= 6; i++)
+      move(i, s[i]);
+     Serial.print(s);
   }
 }
