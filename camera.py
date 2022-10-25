265fc930df3fb3e04f0ddcd3cc44ef816b9a2b33
nnhoa11 <88938984+nnhoa11@users.noreply.github.com>
2022-10-25 21:17:18 +0700

    tested code

--- camera.py
+++ camera.py
@@ -1,13 +1,11 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
-import serial 
-import time 
-from cvzone.SerialModule import SerialObject
+import serial
+from utlis import sendData, connectToRobot
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon = 0.7, maxHands = 1)
-serialcomm = serial.Serial('COM3', 9600)
+serialcomm = serial.Serial('COM5', 9600)
 serialcomm.timeout = 1
-mySerial = SerialObject("COM3", 9600, 1)
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
@@ -15,12 +13,7 @@ while True:
         hand1 = hands[0]
         fingers1 = detector.fingersUp(hand1)
         s = '$' + str(fingers1[0]) + str(fingers1[1]) + str(fingers1[2]) + str(fingers1[3]) + str(fingers1[4]);
-        print(fingers1);
-        # serialcomm.write(s.encode())
-        mySerial.sendData(fingers1)
-        # time.sleep(0.1)
-
-        # print(serialcomm.readline().decode('ascii'))
+        serialcomm.write(s.encode())
     cv2.imshow("image", img)
     key = cv2.waitKey(1)
     if key == ord('q'): break
--- uno/uno.ino
+++ uno/uno.ino
@@ -1,52 +1,107 @@
 #include <Servo.h>
-
-Servo thumb, index, middle, ring, pinky, hand[6];
-const int StringLength = 6;
-int stas[10], counter = 0;
-String received, prev= "$00000";
-bool counterStart = false;
+ 
+Servo servothumb;         
+Servo servoindex;         
+Servo servomiddle;
+Servo servoring;
+Servo servopinky;
+ 
+char number[50];
+char c;
+int state = 0;
+String myStringRec;
+int stringCounter = 0;
+bool stringCounterStart = false;
+String myRevivedString;
+int stringLength = 6;
+ 
+int servoPinky,servoMiddle,servoIndex,servoThumb,servoRing;
+int myVals[] ={0,0,0,0,0} ;
+ 
+int myButton= 1;
+bool portable;
+ 
 void setup() {
-  // put your setup code here, to run once:
+ 
   Serial.begin(9600);
-  thumb.attach(7);
-  index.attach(8);
-  middle.attach(9);
-  ring.attach(10);
-  pinky.attach(11);
-  pinMode(LED_BUILTIN, OUTPUT);
-  thumb.write(0);
+  servothumb.attach(9); 
+  servoindex.attach(10); 
+  servopinky.attach(11);
+  servoring.attach(12);
+  servomiddle.attach(13);
+  myButton = analogRead(A0);
+  delay(500);
+  if (myButton == 0){portable = false;}
+  else {portable = true;}
+   
 }
-void move(int i, char c){
-  if (i == 1) {
-    if (c == '1') thumb.write(180);
-    else thumb.write(0);
-  }
-  else if (i == 2){
-    if (c == '1') 
-    index.write(180);
-    else index.write(0);
-  }
-  else if (i == 3) {
-    if (c == '1') middle.write(180);
-    else middle.write(0);
-  }
-  else if (i == 4) {
-    if (c == '1') ring.write(180);
-    else ring.write(0);
-  }
-  else if (i == 5) {
-    if (c == '1' )pinky.write(180);
-    else pinky.write(0);
-  }
-  
-}
+ 
 void loop() {
-    while (Serial.available() == 0){
-      digitalWrite(LED_BUILTIN, LOW);
+
+  receiveData();
+  if (servoPinky ==1){  servopinky.write(180);}else{servopinky.write(0);}
+  if (servoIndex ==1){  servoindex.write(180);}else{servoindex.write(0);}
+  if (servoMiddle ==1){  servomiddle.write(180);}else{servomiddle.write(0);}
+  if (servoThumb ==1){  servothumb.write(180);}else{servothumb.write(0);}
+  if (servoRing ==1){  servoring.write(180);}else{servoring.write(0);}
+  Serial.write(servoThumb + " " + servoIndex);
+}
+ 
+void allON(){
+   
+servopinky.write(180);
+servoindex.write(180);
+servomiddle.write(180);
+servothumb.write(180);
+servoring.write(180);
+   
+  }
+ 
+ 
+void allOFF(){
+   
+servopinky.write(0);
+servoindex.write(0);
+servomiddle.write(0);
+servothumb.write(0);
+servoring.write(0);
+}
+ 
+ 
+void receiveData() {
+  int i = 0;
+  while (Serial.available()) {
+   char c = Serial.read();
+   
+    if (c == '$') {
+      stringCounterStart = true;
     }
-    String s = Serial.readString();
-    digitalWrite(LED_BUILTIN, HIGH);
-    for (int i = 1; i <= 6; i++)
-      move(i, s[i]);
-    Serial.println(s);
+    if (stringCounterStart == true )
+    {
+      if (stringCounter < stringLength)
+      {
+        myRevivedString = String(myRevivedString + c);
+        stringCounter++;
+      }
+      if (stringCounter >= stringLength) {
+        stringCounter = 0; stringCounterStart = false;
+        servoPinky = myRevivedString.substring(1, 2).toInt();
+        servoRing = myRevivedString.substring(2, 3).toInt();
+        servoMiddle = myRevivedString.substring(3, 4).toInt();
+        servoIndex = myRevivedString.substring(4, 5).toInt();
+        servoThumb = myRevivedString.substring(5, 6).toInt();
+        Serial.write("quay");
+//        Serial.print(servoPinky);
+//        Serial.print(" ");
+//        Serial.print(servoRing);
+//        Serial.print(" ");
+//        Serial.print(servoMiddle);
+//        Serial.print(" ");
+//        Serial.print(servoIndex);
+//        Serial.print(" ");
+//        Serial.println(servoThumb);      
+        myRevivedString = "";
+      }
+    }
+  }
 }
